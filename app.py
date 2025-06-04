4cb9640bd7ba60a0c666e0913a81af8dbc76e594
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-06-04 23:22:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
 
 app = Flask(__name__)
 
@@ -20,4 +15,5 @@ def list_images():
     return {'images': files}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
