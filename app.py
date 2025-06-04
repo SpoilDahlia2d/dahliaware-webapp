6eb02791b2abb43ac6c7aa6254b1f780580e78f7
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-06-04 23:18:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from flask import Flask, render_template, send_from_directory
 import os
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 app = Flask(__name__)
 
 @app.route('/')
