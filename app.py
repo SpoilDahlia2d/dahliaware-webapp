fd3d7c8e109b2ae972695fda7f18771d5c202169
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-06-05 21:29:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 
 @app.route('/')
 def index():
-    user_ip = request.remote_addr
-    return render_template('index.html', ip=user_ip)
-
-@app.route('/get-images')
-def get_images():
     image_dir = os.path.join(app.static_folder, 'images')
-    allowed_exts = {'.jpg', '.jpeg', '.png', '.gif', '.webp'}
-    images = [f for f in os.listdir(image_dir) if os.path.splitext(f)[1].lower() in allowed_exts]
-    return jsonify(images)
+    allowed_ext = ['.png', '.jpg', '.jpeg', '.webp', '.gif']
+    image_files = [f for f in os.listdir(image_dir) if os.path.splitext(f)[1].lower() in allowed_ext]
+    return render_template('index.html', image_files=image_files)
+
+if __name__ == '__main__':
+    app.run()
